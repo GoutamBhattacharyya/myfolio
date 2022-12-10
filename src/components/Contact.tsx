e779daeb46a5c311d96c720416b82d65d3e0254b
GoutamBhattacharyya <info.goutams@gmail.com>
2022-12-10 20:54:37 +0530

    Added validation

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -23,39 +23,48 @@ class ContactForm extends React.Component<{},FormProps> {
         if(!fields["username"]){
             formValid = false;
             error["username"] = "please enter your user name";
-            alert("kkkk")
+        }
+        if(typeof fields["username"] !=="undefined") {
+            if(!fields["username"].match(/^[a-z\s]{0,255}$/i)) {
+                formValid = false;
+                error["username"] = "please enter your user name";
+                
+            }
         }
         this.setState({error:error});
         return(formValid);
     }
     submitRegistration(e:any){
         e.preventDefault();
+        //console.log(this.validForm());
         if(this.validForm()){
-            let fields:any = {};
-            fields["username"] = "";
-            this.setState({fields:fields});
-            alert(this.state.fields.username)
-            
+            let localObj:any = {};
+            localObj["username"] = "";
+            console.log(localObj);
+            this.setState({fields:localObj});
+            console.log(this.state.fields) 
         }        
     }
     handelChange(e:any){
-        const fields = this.state.fields;
-        fields[e.target.name] = e.target.value;
-        this.setState({fields});
+         const fields = this.state.fields;
+         console.log(typeof(fields));
+         fields[e.target.name] = e.target.value;
+         this.setState({fields});
+         console.log(this.state.fields);
     }
     render(): React.ReactNode {
         return (
             <div className="myfolio-contact-section">
                 <form onSubmit={this.submitRegistration} method="post">
-                    <fieldset>
-                        <input type="text" name="username" placeholder="Name" value={this.state.fields.username} onChange={this.handelChange} />
+                    <fieldset>                        
+                        <input type="text" name="username" placeholder="Name" value={this.state.fields.username} onChange= {this.handelChange} />
                         <div className={`"myfolio-error ${this.state.error.username!==undefined?"-show":""}`}>{this.state.error.username}</div>
                     </fieldset>
                     <fieldset>
-                        <input type="email" placeholder="Email" />
+                        <input type="email" name="email" placeholder="Email" value={this.state.fields.email} onChange={this.handelChange} />
                     </fieldset>
                     <fieldset>
-                        <textarea placeholder="Write Your Requirment" />
+                        <textarea name="textarea" placeholder="Write Your Requirment" value={this.state.fields.textarea} onChange={this.handelChange} />
                     </fieldset>
                     <input type={"submit"} className="myfolio-btn" />
                 </form>
